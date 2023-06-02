f79127ae0cb7703e9bc6874bf38f7a54eea365e7
Rebecca Martinho <rebeccamartinho@hotmail.com>
2023-06-02 08:50:25 -0300

    navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-brand href="#">House rules</b-navbar-brand>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">Sobre</b-nav-item>
+          <b-nav-item href="#">Contato</b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Login</b-nav-item>
+          <b-nav-item href="#">Registrar</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
